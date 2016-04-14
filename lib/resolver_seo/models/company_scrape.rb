a91bf794b07fd13bc3a8413c61ea8f926a2024a0
stevecreedon <steve@creedon.me>
2016-04-14 18:14:13 +0100

    added dependent destroy to seo models

--- lib/resolver_seo/models/company_scrape.rb
+++ lib/resolver_seo/models/company_scrape.rb
@@ -2,7 +2,7 @@ module ResolverSeo
   class CompanyScrape
     include Mongoid::Document
     field :term
-    has_many :search_results, class_name: 'ResolverSeo::SearchResult'
+    has_many :search_results, class_name: 'ResolverSeo::SearchResult', dependent: :destroy
     belongs_to :company
     store_in database: :resolver_analytics
   end
